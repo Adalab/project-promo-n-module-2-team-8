d672434eb162014ff58bd2a86df74d48a66fa732
MiriamVF <miriam.verduf@gmail.com>
2021-08-12 11:22:35 +0200

    try collapsables

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,33 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+// // const boxDesign = document.querySelector('js_collapsable_design');
+// // const boxFill = document.querySelector('.js_collapsable_fill');
+// // const boxShare = document.querySelector('.js_collapsable_share');
+// const boxAll = document.querySelector("js__collapsable");
+
+function handleClick(event) {
+  console.log(event);
+  console.log(event.currentTarget);
+}
+
+//   //   const click = event.currentTarget;
+
+//   if (boxDesign.classList.contains("hidden")) {
+//     boxDesign.classList.remove("hidden");
+//   } else {
+//     boxDesign.classList.add("hidden");
+//   }
+
+//   console.log(event);
+// }
+
+// // boxAll.addEventListener("click", handleClick);
+
+// document
+//   .getElementsByClassName("js__collapsable")
+//   .addEventListener("click", handleClick);
+
+// boxDesign.addEventListener('click', handleClick);
+
+// boxFill.addEventListener('click', handleClick);
+// boxShare.addEventListener('click', handleClick);
