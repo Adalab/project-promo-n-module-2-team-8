a73ffd739205c11ca69831c7a9886805012c0ae9
sasalinero <sasalinero@gmail.com>
2021-09-04 17:06:47 +0200

    preview

--- src/js/main.js
+++ src/js/main.js
@@ -12,25 +12,46 @@ function handleClick(ev) {
     const fieldset = ev.currentTarget.parentNode.parentNode.querySelector(".js__fieldset");
     fieldset.classList.toggle("hidden");
 }
-// let data{
-//     {
-//         palette: ,
-//         name: "",
-//         job: "",
-//         phone: "",
-//         email: "",z
-//         linkedin: "",
-//         github: "",
-//         photo: ""
-//       }
-// }
+
 const form =document.querySelector(".form");
 
 function handlerFormData(ev){
 
     const inputId= ev.target.id;
+    
     const inputValue= ev.target.value;
+    if (inputId === "fullname"){
 
+        document.querySelector(".photo__card--name").innerHTML= inputValue;
+        
+    }
+    else if(inputId === "job"){
+
+        document.querySelector(".photo__card--frontend").innerHTML= inputValue;
+
+    }
+    else if(inputId === "telFill"){
+
+        document.querySelector("#telLink").href= "tel:"+inputValue;
+
+    }
+  
+    else if(inputId === "emailFill"){
+
+        document.querySelector("#emailLink").href= "mailto:"+inputValue;
+
+    }
+    else if(inputId === "linkedinFill"){
+
+        document.querySelector("#linkedinLink").href= "https://www.linkedin.com/in/:"+inputValue;
+
+    }
+    else if(inputId === "githubFill"){
+
+        document.querySelector("#githubLink").href= "https://github.com/"+inputValue;
+
+    }
+    
     console.log(inputId, inputValue);
 }
 
