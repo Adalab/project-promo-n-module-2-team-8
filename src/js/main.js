3c807c030979527da859c9bbd5c5627a676251d2
Paloma <paloma.arevalog@gmail.com>
2021-09-05 17:28:11 +0200

    change colors js added

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-undef */
+/* eslint-disable indent */
+/* eslint-disable quotes */
 "use strict";
 
 const collapsable = document.querySelectorAll(".js__collapsable");
@@ -9,6 +12,33 @@ for (let i = 0; i < collapsable.length; i++) {
 function handleClick(ev) {
     ev.currentTarget.classList.toggle("form__arrow--rotate");
 
-    const fieldset = ev.currentTarget.parentNode.parentNode.querySelector(".js__fieldset");
+    const fieldset =
+        ev.currentTarget.parentNode.parentNode.querySelector(".js__fieldset");
     fieldset.classList.toggle("hidden");
 }
+// palette selection
+
+const photoPalette = document.querySelector(".js-photo");
+const palette1 = document.querySelector(".js-palette-1");
+const palette2 = document.querySelector(".js-palette-2");
+const palette3 = document.querySelector(".js-palette-3");
+
+function changeColors(event) {
+    if (event.currentTarget.value === "palette-1") {
+        photoPalette.classList.add("palette-1");
+        photoPalette.classList.remove("palette-2");
+        photoPalette.classList.remove("palette-3");
+    } else if (event.currentTarget.value === "palette-2") {
+        photoPalette.classList.remove("palette-1");
+        photoPalette.classList.add("palette-2");
+        photoPalette.classList.remove("palette-3");
+    } else if (event.currentTarget.value === "palette-3") {
+        photoPalette.classList.remove("palette-1");
+        photoPalette.classList.remove("palette-2");
+        photoPalette.classList.add("palette-3");
+    }
+}
+
+palette1.addEventListener("click", changeColors);
+palette2.addEventListener("click", changeColors);
+palette3.addEventListener("click", changeColors);
