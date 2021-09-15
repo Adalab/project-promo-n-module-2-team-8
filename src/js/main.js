9f97fa4ccf2305a2e265bea4699075fb2451435b
MiriamVF <miriam.verduf@gmail.com>
2021-09-15 11:27:26 +0200

    final version

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-undef */
-/* eslint-disable indent */
-/* eslint-disable quotes */
 'use strict';
 
 //**************************** COLLAPSABLE*********************************//
@@ -14,62 +11,57 @@ const share = document.querySelector('.dropdown');
 const shareArrow = document.querySelector('.share__arrow');
 
 for (let i = 0; i < collapsable.length; i++) {
-    collapsable[i].addEventListener('click', handleClick);
+  collapsable[i].addEventListener('click', handleClick);
 }
 
 function handleClick(ev) {
-    ev.currentTarget.classList.toggle("form__arrow--rotate");
-    const fieldset =
-        ev.currentTarget.parentNode.parentNode.querySelector(".js__fieldset");
-    console.log(fieldset.parentNode.classList.value);
-    fieldset.classList.toggle("hidden");
-    const removeSelector = fieldset.parentNode.classList;
-    console.log(fieldset.classList);
+  ev.currentTarget.classList.toggle('form__arrow--rotate');
+  const fieldset =
+    ev.currentTarget.parentNode.parentNode.querySelector('.js__fieldset');
+  fieldset.classList.toggle('hidden');
+  const removeSelector = fieldset.parentNode.classList;
 
-    if (removeSelector.value === 'design__legend') {
-        fill.classList.add('hidden');
-        fillArrow.classList.remove("form__arrow--rotate");
-        share.classList.add('hidden');
-        shareArrow.classList.remove("form__arrow--rotate");
-    }
-    else if (removeSelector.value === 'fill') {
-        design.classList.add('hidden');
-        designArrow.classList.remove("form__arrow--rotate");
-        share.classList.add('hidden');
-        shareArrow.classList.remove("form__arrow--rotate");
-    }
-    else if (removeSelector.value === 'share') {
-        design.classList.add('hidden');
-        designArrow.classList.remove("form__arrow--rotate");
-        fill.classList.add('hidden');
-        fillArrow.classList.remove("form__arrow--rotate");
-    }
-
+  if (removeSelector.value === 'design__legend') {
+    fill.classList.add('hidden');
+    fillArrow.classList.remove('form__arrow--rotate');
+    share.classList.add('hidden');
+    shareArrow.classList.remove('form__arrow--rotate');
+  } else if (removeSelector.value === 'fill') {
+    design.classList.add('hidden');
+    designArrow.classList.remove('form__arrow--rotate');
+    share.classList.add('hidden');
+    shareArrow.classList.remove('form__arrow--rotate');
+  } else if (removeSelector.value === 'share') {
+    design.classList.add('hidden');
+    designArrow.classList.remove('form__arrow--rotate');
+    fill.classList.add('hidden');
+    fillArrow.classList.remove('form__arrow--rotate');
+  }
 }
 //**************************** FILL*********************************//
 
 const form = document.querySelector('.form');
 
 function handlerFormData(ev) {
-    const inputId = ev.target.id;
-    const inputValue = ev.target.value;
-    if (inputId === 'fullname') {
-        document.querySelector('.photo__card--name').innerHTML = inputValue;
-    } else if (inputId === 'job') {
-        document.querySelector('.photo__card--frontend').innerHTML = inputValue;
-    } else if (inputId === 'telFill') {
-        document.querySelector('#telLink').href = 'tel:' + inputValue;
-    } else if (inputId === 'emailFill') {
-        document.querySelector('#emailLink').href = 'mailto:' + inputValue;
-    } else if (inputId === 'linkedinFill') {
-        document.querySelector('#linkedinLink').href =
-            'https://www.linkedin.com/in/:' + inputValue;
-    } else if (inputId === 'githubFill') {
-        document.querySelector('#githubLink').href =
-            'https://github.com/' + inputValue;
-    }
-    saveLocalStorage();
-    //console.log(inputId, inputValue);
+  const inputId = ev.target.id;
+  const inputValue = ev.target.value;
+  if (inputId === 'fullname') {
+    document.querySelector('.photo__card--name').innerHTML = inputValue;
+  } else if (inputId === 'job') {
+    document.querySelector('.photo__card--frontend').innerHTML = inputValue;
+  } else if (inputId === 'telFill') {
+    document.querySelector('#telLink').href = 'tel:' + inputValue;
+  } else if (inputId === 'emailFill') {
+    document.querySelector('#emailLink').href = 'mailto:' + inputValue;
+  } else if (inputId === 'linkedinFill') {
+    document.querySelector('#linkedinLink').href =
+      'https://www.linkedin.com/in/:' + inputValue;
+  } else if (inputId === 'githubFill') {
+    document.querySelector('#githubLink').href =
+      'https://github.com/' + inputValue;
+  }
+  saveLocalStorage();
+  //console.log(inputId, inputValue);
 }
 
 form.addEventListener('change', handlerFormData);
@@ -82,29 +74,25 @@ const palette2 = document.querySelector('.js-palette-2');
 const palette3 = document.querySelector('.js-palette-3');
 
 function changeColors(event) {
-    if (event.currentTarget.value === '1') {
-        photoPalette.classList.add('palette-1');
-        photoPalette.classList.remove('palette-2');
-        photoPalette.classList.remove('palette-3');
-    } else if (event.currentTarget.value === '2') {
-        photoPalette.classList.remove('palette-1');
-        photoPalette.classList.add('palette-2');
-        photoPalette.classList.remove('palette-3');
-    } else if (event.currentTarget.value === '3') {
-        photoPalette.classList.remove('palette-1');
-        photoPalette.classList.remove('palette-2');
-        photoPalette.classList.add('palette-3');
-    }
-    saveLocalStorage();
+  if (event.currentTarget.value === '1') {
+    photoPalette.classList.add('palette-1');
+    photoPalette.classList.remove('palette-2');
+    photoPalette.classList.remove('palette-3');
+  } else if (event.currentTarget.value === '2') {
+    photoPalette.classList.remove('palette-1');
+    photoPalette.classList.add('palette-2');
+    photoPalette.classList.remove('palette-3');
+  } else if (event.currentTarget.value === '3') {
+    photoPalette.classList.remove('palette-1');
+    photoPalette.classList.remove('palette-2');
+    photoPalette.classList.add('palette-3');
+  }
+  saveLocalStorage();
 }
 
 palette1.addEventListener('click', changeColors);
 palette2.addEventListener('click', changeColors);
 palette3.addEventListener('click', changeColors);
-
-function handleGetImage(e) {
-    console.log(e);
-}
 
 //**************************** image preview*********************************//
 
@@ -116,7 +104,7 @@ const profileImage = document.querySelector('.photo__card--photo');
 const profilePreview = document.querySelector('.photo__square');
 
 /**
- * Recoge el archivo añadido al campo de tipo "file"
+ * Recoge el archivo añadido al campo de tipo 'file'
  * y lo carga en nuestro objeto FileReader para que
  * lo convierta a algo con lo que podamos trabajar.
  * Añade un listener al FR para que ejecute una función
@@ -124,9 +112,9 @@ const profilePreview = document.querySelector('.photo__square');
  * @param {evento} e
  */
 function handleGetImage(e) {
-    const myFile = e.currentTarget.files[0];
-    fr.addEventListener('load', writeImage);
-    fr.readAsDataURL(myFile);
+  const myFile = e.currentTarget.files[0];
+  fr.addEventListener('load', writeImage);
+  fr.readAsDataURL(myFile);
 }
 
 /**
@@ -134,18 +122,18 @@ function handleGetImage(e) {
  * trabajar con ellos ;)
  */
 function writeImage() {
-    /* En la propiedad `result` de nuestro FR se almacena
-     * el resultado. Ese resultado de procesar el fichero que hemos cargado
-     * podemos pasarlo como background a la imagen de perfil y a la vista previa
-     * de nuestro componente.
-     */
-    profileImage.style.backgroundImage = `url(${fr.result})`;
-    profilePreview.style.backgroundImage = `url(${fr.result})`;
-    saveLocalStorage();
+  /* En la propiedad `result` de nuestro FR se almacena
+   * el resultado. Ese resultado de procesar el fichero que hemos cargado
+   * podemos pasarlo como background a la imagen de perfil y a la vista previa
+   * de nuestro componente.
+   */
+  profileImage.style.backgroundImage = `url(${fr.result})`;
+  profilePreview.style.backgroundImage = `url(${fr.result})`;
+  saveLocalStorage();
 }
 
 /**
- * Genera un click automático en nuesto campo de tipo "file"
+ * Genera un click automático en nuesto campo de tipo 'file'
  * que está oculto
  */
 /*function fakeFileClick() {
@@ -163,37 +151,36 @@ getFile.addEventListener('change', handleGetImage);
 //**************************** localStorage*********************************//
 
 let storageArray = {
-    palette: '',
-    fullname: '',
-    job: '',
-    emailFill: '',
-    telFill: '',
-    linkedinFill: '',
-    githubFill: '',
-    imgselector: '',
+  palette: '',
+  fullname: '',
+  job: '',
+  emailFill: '',
+  telFill: '',
+  linkedinFill: '',
+  githubFill: '',
+  imgselector: '',
 };
-const formStorage = document.querySelector('.js-form');
+
 
 function handlerFormStorage(ev) {
-    const inputId = ev.target.id;
-    const inputValue = ev.target.value;
-    storageArray[inputId] = inputValue;
-    console.log(storageArray);
+  const inputId = ev.target.id;
+  const inputValue = ev.target.value;
+  storageArray[inputId] = inputValue;
 }
 form.addEventListener('change', handlerFormStorage);
 
 function saveLocalStorage() {
-    localStorage.setItem('storageArray', JSON.stringify(storageArray));
+  localStorage.setItem('storageArray', JSON.stringify(storageArray));
 }
 
 function getLocalStorage() {
-    //Obtenemos lo que hay en el local storage
-    const personalInfo = localStorage.getItem('storageArray');
-    //preguntamos si lo que me ha devuelto está vacío o no
-    if (personalInfo !== null) {
-        const arrayLocalStorage = JSON.parse(personalInfo);
-        storageArray = arrayLocalStorage;
-    }
+  //Obtenemos lo que hay en el local storage
+  const personalInfo = localStorage.getItem('storageArray');
+  //preguntamos si lo que me ha devuelto está vacío o no
+  if (personalInfo !== null) {
+    const arrayLocalStorage = JSON.parse(personalInfo);
+    storageArray = arrayLocalStorage;
+  }
 }
 
 getLocalStorage();
@@ -202,7 +189,7 @@ getLocalStorage();
 const resetButton = document.querySelector('.photo__reset--container');
 
 function handleReset() {
-    location.reload();
+  location.reload();
 }
 
 resetButton.addEventListener('click', handleReset);
